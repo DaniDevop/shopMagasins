84c057864b5eb0b2e2c2c49347a1b5ed3ed00d3b
Daniel <DanielLevy@gmail.com>
2024-07-26 12:04:25 +0000

    install templates admin

---
+++ resources/views/admin/pages/head.blade.php
@@ -0,0 +1,15 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Regal Admin</title>
+    <link rel="stylesheet" href="{{asset('/admin/vendors/mdi/css/materialdesignicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('/admin/vendors/feather/feather.css')}}">
+    <link rel="stylesheet" href="{{asset('/admin/vendors/base/vendor.bundle.base.css')}}">
+
+    <link rel="stylesheet" href="{{asset('/admin/vendors/flag-icon-css/css/flag-icon.min.css')}}"/>
+    <link rel="stylesheet" href="{{asset('/admin/vendors/font-awesome/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('/admin/vendors/jquery-bar-rating/fontawesome-stars-o.css')}}">
+    <link rel="stylesheet" href="{{asset('/admin/vendors/jquery-bar-rating/fontawesome-stars.css')}}">
+
+    <link rel="stylesheet" href="{{asset('/admin/css/style.css')}}">
+  </head>
---
+++ resources/views/admin/pages/js.blade.php
@@ -0,0 +1,10 @@
+ <script src="{{asset('/admin/vendors/base/vendor.bundle.base.js')}}"></script>
+
+ <script src="{{asset('/admin/js/off-canvas.js')}}"></script>
+ <script src="{{asset('/admin/js/hoverable-collapse.js')}}"></script>
+ <script src="{{asset('/admin/js/template.js')}}"></script>
+
+ <script src="{{asset('/admin/vendors/chart.js/Chart.min.js')}}"></script>
+ <script src="{{asset('/admin/vendors/jquery-bar-rating/jquery.barrating.min.js')}}"></script>
+
+ <script src="{{asset('/admin/js/dashboard.j')}}s"></script>
